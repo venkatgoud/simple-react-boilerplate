738be17b8e20f5d5c0f207e59a5f3c11712ecf3c
venkatgoud <venkat.goud@gmail.com>
2016-07-26 21:24:35 +0530

    Minor changes

--- js/routes/index.js
+++ js/routes/index.js
@@ -1,7 +1,7 @@
 import React	from 'react';
 import { IndexRoute, Route } from 'react-router';
 import Main 	from '../containers/Main';
-import BoardContainer 	from '../containers/SampleContainer';
+import SampleContainer 	from '../containers/SampleContainer';
 import Users 	from '../components/Users';
 
 var configureRoutes = () => {
